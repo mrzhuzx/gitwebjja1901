5b2351e6efb390b19ec6d75dfac731b335ade6c7
zxowl <zxowl@qq.com>
2019-05-14 15:02:20 +0800

    添加笔记 --tzhu--time

---
+++ src/main/java/com/tzhu/javaweb/TestGit001.java
@@ -0,0 +1,14 @@
+package com.tzhu.javaweb;
+
+public class TestGit001 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("AAAAAAAAAAAAAAAAAAAA");
+        System.out.println("AAAAAAAAAAAAAAAAAAAA");
+        System.out.println("AAAAAAAAAAAAAAAAAAAA");
+        System.out.println("AAAAAAAAAAAAAAAAAAAA");
+
+    }
+}
